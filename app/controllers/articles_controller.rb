da7146549e7e30fd967da5fcc0493e4cf822388f
Akalugo Daniel <akalugidaniel@gmail.com>
2022-07-20 20:24:04 +0100

    Added help messages regarding article creations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,23 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find_by(id: params[:id])
     end
+    def new
+        @article = Article.new
+    end
+     def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:success] = "Article was created successfully"
+            redirect_to @article
+        else
+            render 'new'
+        end
+        
+        
+     end
 
+     private
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
 end
