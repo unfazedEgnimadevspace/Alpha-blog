a4e707b86b66cdac30b7194c24c765da190ef6df
Akalugo Daniel <akalugidaniel@gmail.com>
2022-07-20 22:47:40 +0100

    Did a little refactoring to the article controllers and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,21 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+
     def index
         @articles = Article.all
     end
+
     def show
-        @article = Article.find_by(id: params[:id])
     end
+
+
     def new
         @article = Article.new
     end
-     def create
+    def edit
+    end
+
+    def create
         @article = Article.new(article_params)
         if @article.save
             flash[:success] = "Article was created successfully"
@@ -16,11 +23,26 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
-        
-        
-     end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:success] ="Article updated sucessfully"
+            redirect_to @article
+        else
+            render "edit"
+        end
+    end
+    
+    def destroy
+        @article.destroy
+        redirect_to articles_url
+    end
 
      private
+        def set_article
+            @article = Article.find_by(id: params[:id])
+        end
         def article_params
             params.require(:article).permit(:title, :description)
         end
