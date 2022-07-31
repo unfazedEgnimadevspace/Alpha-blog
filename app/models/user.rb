8216b93788d76f1001d91aa17ec1462fcbce1324
Akalugo Daniel <akalugidaniel@gmail.com>
2022-07-31 20:54:23 +0100

    made sure the email is downcased before being saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { email.downcase! }
     has_many :articles
     validates :username, presence: true, length: { minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
