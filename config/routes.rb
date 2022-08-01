203f720cdc43e6c56ffc53df4e4cf5a50433c921
Akalugo Daniel <akalugidaniel@gmail.com>
2022-08-01 01:21:10 +0100

    enabled users to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "pages#home"
   get "about", to: "pages#about"
+  get 'signup', to: "users#new"
   resources :articles
+  resources :users, execept: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_191140) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_203606) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_191140) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
