fefacd146d6970b103e3d3c8bfd407de692c27cd
Akalugo Daniel <akalugidaniel@gmail.com>
2022-08-01 11:23:06 +0100

    Added user edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user =  User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your profile  was edited successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
 
-
+  end
 
 
   private
