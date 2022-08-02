a336fe2c18d902db5247808eaefad187409c116a
Akalugo Daniel <akalugidaniel@gmail.com>
2022-08-02 11:59:07 +0100

    Added security features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    def require_user
+        if !logged_in?
+            flash[:danger] = "You must be logged in to perform that action"
+            redirect_to login_path
+        end
+    end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [ :show, :edit, :update, :destroy ]
-
+    before_action :require_user, execept: [ :index, :show]
+    before_action :require_same_user , only: [ :edit, :update, :destroy ]
     def index
         @articles = Article.paginate(page: params[:page], per_page: 2)
     end
@@ -47,5 +48,11 @@ class ArticlesController < ApplicationController
         end
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "Access denied"
+                redirect_to @article
+            end
         end
 end
